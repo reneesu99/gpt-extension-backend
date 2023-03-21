c922c02dbfdb1e7826f5f76edd1cf1b8ee4023b3
reneesu99 <reneesu22@gmail.com>
2023-03-20 22:28:25 -0700

    connected vercel environment variables

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const token = ''
+const token = process.env.TOKEN
 app.use(express.json())    // <==== parse request body as JSON
 
 
